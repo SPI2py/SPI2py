49c039d94f70dfb709f44130f1fbf06992be26a2
Chad-Peterson <97306485+Chad-Peterson@users.noreply.github.com>
2023-01-20 10:55:52 -0600

    Continue merging files

--- tests/test_analysis/test_distance_calculations/test_min_line_line_distance.py
+++ tests/test_analysis/test_distance_calculations/test_min_line_line_distance.py
@@ -1,34 +1,113 @@
-# import numpy as np
-# from utils.distance_calculations import min_line_line_distance
-#
-# # Add test to this and other dist for list vs np array
-#
-#
-# def test_parallel():
-#
-#     a0 = np.array([0., 0., 0.])
-#     a1 = np.array([1., 0., 0.])
-#     b0 = np.array([0., 0., 1.])
-#     b1 = np.array([1., 0., 1.])
-#
-#     dist = min_line_line_distance(a0, a1, b0, b1)
-#
-#     print('dist', dist)
-#
-#     assert round(dist,2) == 1
-#     #Add more tests
-#
-#
-# def test_skew():
-#
-#     a0 = np.array([0., 0., 0.])
-#     a1 = np.array([1., 0., 0.])
-#     b0 = np.array([0., 0., 2.])
-#     b1 = np.array([1., 0., 1.])
-#
-#     dist = min_line_line_distance(a0, a1, b0, b1)
-#
-#     print('dist', dist)
-#
-#     assert round(dist,2) == 1
-#
+import numpy as np
+from src.SPI2Py.analysis.distance import min_linesegment_linesegment_distance
+
+# Add test to this and other dist for list vs np array
+
+
+def test_parallel():
+
+    #test single dimension
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([1., 0., 0.])
+    b0 = np.array([0., 0., 1.])
+    b1 = np.array([1., 0., 1.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 1
+
+    #Zane to do: add more tests
+    #https://www.cliffsnotes.com/study-guides/geometry/parallel-lines/testing-for-parallel-lines
+
+    #test 3 dimensions
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([8., 8., 8.])
+    b0 = np.array([1., 1., 0.])
+    b1 = np.array([9., 9., 8.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == round(0.8164965809277261, 2)
+
+    #test parallel line segments overlapping
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([0., 0., 4.])
+    b0 = np.array([0., 0., 2.])
+    b1 = np.array([0., 0., 5.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 0
+
+    #test line segments on same line but not overlapping
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([0., 1., 0.])
+    b0 = np.array([0., 2., 0.])
+    b1 = np.array([0., 4., 0.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 1
+
+def test_skew():
+
+    #test two endpoints closest point two dimensions
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([1., 0., 0.])
+    b0 = np.array([0., 0., 2.])
+    b1 = np.array([1., 0., 1.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 1
+
+    #test two midsections closest point two dimensions
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([3., 3., 0.])
+    b0 = np.array([0., 3., 1.])
+    b1 = np.array([3., 0., 1.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 1
+
+    #test midsection closest to endpoint
+    a0 = np.array([0., 0., 0.])
+    a1 = np.array([2., 2., 2.])
+    b0 = np.array([2., 0., 3.])
+    b1 = np.array([2., 4., 3.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 1
+
+    #test negative points
+    a0 = np.array([-1., -2., 0.])
+    a1 = np.array([-1., -2., -3.])
+    b0 = np.array([-3., -2., 0.])
+    b1 = np.array([-5., -8., -3.])
+
+    dist = min_linesegment_linesegment_distance(a0, a1, b0, b1)
+
+    print('dist', dist)
+
+    assert round(dist,2) == 2
+
+
+
+test_parallel()
+test_skew()
--- tests/test_analysis/test_distance_calculations/test_min_point_line_distance.py
+++ tests/test_analysis/test_distance_calculations/test_min_point_line_distance.py
@@ -1,41 +1,114 @@
-# import numpy as np
-# from utils.distance_calculations import min_point_line_distance
+import numpy as np
+import math
+
+from src.SPI2Py.analysis.distance import min_spheres_linesegment_distance
+
+# For all tests check gradient eval too..
+
+def test_first():
+    #test simple orthogonal endpoint case
+
+    p = np.array([2., 1., 2.])
+    a = np.array([1., 1., 1.])
+    b = np.array([2., 1., 1.])
+
+    # p = np.array([1., 0., 1.])
+    # a = np.array([0., 0., 0.])
+    # b = np.array([1., 0., 0.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == 1
+
+    #test point equals endpoint case
+    p = np.array([4., 5., 6.])
+    a = np.array([0., 0., 0.])
+    b = np.array([4., 5., 6.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == 0
+
+    #test point equals midpoint case
+    p = np.array([2., 2., 5.])
+    a = np.array([2., 2., 2.])
+    b = np.array([2., 2., 8.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == 0
+
+    #test midpoint orthogonal case
+    p = np.array([2., 0., 2.])
+    a = np.array([0., 0., 0.])
+    b = np.array([4., 0., 0.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == 2
+
+    #test within same line but not on line segment
+    p = np.array([8., 0., 0.])
+    a = np.array([0., 0., 0.])
+    b = np.array([5., 0., 0.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == 3
+
+    #test far away, endpoint closest
+    p = np.array([3., 3., 3.])
+    a = np.array([0., 0., 0.])
+    b = np.array([-1., 0., 0.])
+
+    min_dist = min_spheres_linesegment_distance(p, a, b)
+
+    print("min_dist: ", min_dist)
+
+    assert round(min_dist, 2) == round(3 * math.sqrt(3), 2)
+
+
+
+# test_first()
+
+
+
+
+# Speed test saved...
+# p = np.array([0., 1., 1.])
+# a = np.array([0., 0., 0.])
+# b = np.array([0., 0., 1.])
 #
-# # For all tests check gradient eval too..
+# aa = np.array([[0., 0., 0.]])
+# ab = np.array([[0., 0., 0.],
+#                [0., 0., 1.],
+#                [0., 0., 1.],
+#                [0., 0., 1.],
+#                [0., 0., 1.]])
 #
-# def test_first():
-#     p = np.array([1., 0., 1.])
-#     a = np.array([0., 0., 0.])
-#     b = np.array([1., 0., 0.])
+# start = perf_counter_ns()
+# print(min_point_line_distance(p, a, b))
+# stop = perf_counter_ns()
+# print('Time', stop-start)
 #
-#     min_dist = min_point_line_distance(p, a, b)
+# start = perf_counter_ns()
+# print(min_point_line_distance(p, a, b))
+# stop = perf_counter_ns()
+# print('Time', stop-start)
 #
+# start = perf_counter_ns()
+# print(min_cdist(aa, ab))
+# stop = perf_counter_ns()
+# print('Time', stop-start)
 #
-# # Speed test saved...
-# # p = np.array([0., 1., 1.])
-# # a = np.array([0., 0., 0.])
-# # b = np.array([0., 0., 1.])
-# #
-# # aa = np.array([[0., 0., 0.]])
-# # ab = np.array([[0., 0., 0.],
-# #                [0., 0., 1.],
-# #                [0., 0., 1.],
-# #                [0., 0., 1.],
-# #                [0., 0., 1.]])
-# #
-# # start = perf_counter_ns()
-# # print(min_point_line_distance(p, a, b))
-# # stop = perf_counter_ns()
-# # print('Time', stop-start)
-# #
-# # start = perf_counter_ns()
-# # print(min_point_line_distance(p, a, b))
-# # stop = perf_counter_ns()
-# # print('Time', stop-start)
-# #
-# # start = perf_counter_ns()
-# # print(min_cdist(aa, ab))
-# # stop = perf_counter_ns()
-# # print('Time', stop-start)
-# #
-# # # print(min_cdist(p, ab))
+# # print(min_cdist(p, ab))
